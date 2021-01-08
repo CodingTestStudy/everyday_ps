52bf6222b8c1d8417767b32696bef2052ed443b5
songbae <overwatch94@naver.com>
2021-01-08 09:40:38 +0900

    2021-01-08

--- bronze.cpp
+++ bronze.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cstring>
-#include <queue>
-#include <string>
-#define MAX 100001
+
 using namespace std;
-int idx;
-int N;
-char  moo(int num) {
-	if (num == 1) {
-		if (idx + 1 == N)return 'm';
-		else if (idx + 2 == N || idx + 3 == N)return 'o';
-		idx += 3;
-		return NULL;
+bool check[10];
+int ans;
+void dfs(int idx) {
+	if (idx == 5) {
+		for (int i = 0; i < 5; i++) {
+			if (!check[i])continue;
+			cout << i << " ";
+		}
+		cout << endl;
+		ans++;
+		return;
 	}
-	moo(num - 1);
-	if (idx + 1 == N)return 'm';
-	idx++;
-	for (int i = 0; i < num; i++) {
-		if (idx + 1 == N)return'o';
-		idx++;
-	}
-	return moo(num - 1);
+	dfs(idx + 1);
+	check[idx] = true;
+	dfs(idx + 1);
+	check[idx] = false;
 }
 int main() {
-	cin >> N;
-	cout << moo(30);
+	dfs(0);
+	cout << ans;
 }
